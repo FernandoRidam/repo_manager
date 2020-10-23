cd3539dd6f6683a94101289b666b23e90a445077
FernandoRidam <fernandohipalae71@gmail.com>
2020-10-23 09:57:42 -0300

    Configurações e arquitetura de pastas

---
+++ src/pages/Auth/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+export function Auth() {
+  return (
+    <div className="auth-container">
+      <div className="auth-card"></div>
+    </div>
+  );
+};
