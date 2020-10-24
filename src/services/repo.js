44668ea0e600c1efe6b27eaa5b1f2ccba1ecd5e7
FernandoRidam <fernandohipalae71@gmail.com>
2020-10-24 19:55:27 -0300

    configurações dos services

---
+++ src/services/repo.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const GET_REPO = gql`
+  query getRepo($owner: String!, $name: String!) {
+    repository(owner: $owner, name: $name){
+      pullRequests(
+        first: 100,
+        orderBy: { field: CREATED_AT, direction: DESC },
+      ) {
+          nodes {
+            title,
+            author {
+              login,
+            },
+            state,
+            url,
+          }
+      }
+    }
+  }
+`;
